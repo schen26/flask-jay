9988e11950240c06735fa2f84e1ea0e542b8c628
Jordan Grindrod <jlgrindrod@hotmail.com>
2019-11-26 14:04:53 +0000

    finished app up to stage needed

--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 from application import app, db
 from application.models import Posts
+from application.forms import PostForm
 
 @app.route('/')
 @app.route('/home')
@@ -11,3 +12,23 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+@app.route('/post', methods=['GET','POST'])
+def post():
+    form = PostForm()
+    if form.validate_on_submit():
+        postData = Posts(
+                first_name = form.first_name.data,
+                last_name = form.last_name.data,
+                title = form.title.data,
+                content = form.content.data
+        )
+        
+        db.session.add(postData)
+        db.session.commit()
+    
+        return redirect(url_for('home'))
+    else:
+        print(form.errors)
+    
+    return render_template('post.html', title='Post', form=form)
